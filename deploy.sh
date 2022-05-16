6bb25f97cfc08be9e2d4b15769e411e9320bc740
ndtvinh13 <ndtvinh14@gmail.com>
2022-05-15 19:10:24 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,9 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+git add dist -f
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/ndtvinh13/reaction_timer.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 cd -
